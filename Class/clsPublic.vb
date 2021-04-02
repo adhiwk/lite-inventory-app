e6c4c5f49ecea91f4c9168fc9b42f65fd13d0201
adhiwk <info@indovb.com>
2021-04-02 21:34:35 +0800

    my lite inventory system

---
+++ Class/clsPublic.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Data.OleDb
+Public Class clsPublic
+    Public Shared Function FileAccess() As OleDbConnection
+        Dim connectionString As String =
+                  "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" &
+                  Application.StartupPath & "\qrcode.mdb;Jet OLEDB:Database Password=Masuk123!@#"
+        ';Extended Properties=Text;HDR=YES;FMT=Delimeted;IMEX=1;Database Password=" & _
+        'My.Settings.Local_DB.ToString.Trim & ""
+
+        'Dim ConnectionString As String =
+        '    "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" &
+        '    Application.StartupPath &
+        '    "\qrcode.accdb;Persist Security Info=False;"
+
+        Return New OleDbConnection(connectionString)
+    End Function
+
+    Public Shared Function ConvertTanggal(ByVal dtTanggal As DevExpress.XtraEditors.DateEdit) As String
+        Dim cBulan As String = ""
+        Dim cTanggal As String = ""
+        'Dim cTanggalKunjungan As String = ""
+        If Len(dtTanggal.DateTime.Month.ToString.Trim) = 1 Then
+            cBulan = "0" & dtTanggal.DateTime.Month.ToString.Trim
+        Else
+            cBulan = dtTanggal.DateTime.Month.ToString.Trim
+        End If
+
+        If Len(dtTanggal.DateTime.Day.ToString.Trim) = 1 Then
+            cTanggal = "0" & dtTanggal.DateTime.Day.ToString.Trim
+        Else
+            cTanggal = dtTanggal.DateTime.Day.ToString.Trim
+        End If
+        ConvertTanggal = dtTanggal.DateTime.Year.ToString.Trim & "-" & cBulan.Trim & "-" & cTanggal.Trim
+    End Function
+
+End Class
---
+++ Form/frmLaporan.vb
@@ -0,0 +1,64 @@
+﻿Imports CrystalDecisions.CrystalReports.Engine
+Imports CrystalDecisions.Shared
+
+Public Class frmLaporan
+    Private Sub frmLaporan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.WindowState = FormWindowState.Maximized
+    End Sub
+
+    Public Function CetakQrCode(ByVal cReportName As String,
+                                ByVal cFormula As String,
+                                ByVal cDBName As String) As Boolean
+        Dim cryRpt As New ReportDocument
+        Dim crtableLogoninfos As New TableLogOnInfos
+        Dim crtableLogoninfo As New TableLogOnInfo
+        Dim crConnectionInfo As New ConnectionInfo
+        Dim CrTables As Tables
+        Dim CrTable As Table
+
+        cryRpt.Load(String.Format("{0}\report\{1}", Application.StartupPath, cReportName))
+
+        With crConnectionInfo
+            .Type = ConnectionInfoType.DBFile
+            .ServerName = Application.StartupPath & "\" & Trim(cDBName.Trim)
+            .DatabaseName = Application.StartupPath & "\" & Trim(cDBName.Trim)
+            .UserID = ""
+            .Password = "Masuk123!@#"
+        End With
+
+        CrTables = cryRpt.Database.Tables
+        For Each CrTable In CrTables
+            crtableLogoninfo = CrTable.LogOnInfo
+            crtableLogoninfo.ConnectionInfo = crConnectionInfo
+            CrTable.ApplyLogOnInfo(crtableLogoninfo)
+        Next
+
+        'cryRpt.SetParameterValue("tglperiksa", cTglPerika.Trim)
+        'cryRpt.SetParameterValue("umur", cUmur.Trim)
+
+        crViewer.ReportSource = cryRpt
+        If cFormula.Trim <> "0" Then
+            crViewer.SelectionFormula = cFormula.Trim
+        End If
+        crViewer.Refresh()
+        Return True
+    End Function
+
+    Private Sub frmLaporan_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.P Then
+            crViewer.PrintReport()
+        End If
+    End Sub
+
+    Private Sub frmLaporan_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
+        Me.BringToFront()
+        Me.WindowState = FormWindowState.Maximized
+    End Sub
+
+    Private Sub crViewer_Load(sender As Object, e As EventArgs)
+        If String.IsNullOrEmpty(crViewer.ReportSource.ToString.Trim) Then
+            crViewer.Dispose()
+        End If
+    End Sub
+
+End Class
